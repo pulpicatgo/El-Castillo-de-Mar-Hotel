b6f3e00638f8efeab0f515a32b570bc021eba6ce
Angel <>
2021-05-19 15:44:13 -0500

    prueba

--- prueba4/src/prueba4/prueba4.java
+++ prueba4/src/prueba4/prueba4.java
@@ -5,6 +5,7 @@ package prueba4;
 public class prueba4 {
     public static void main(String[] args) {
         System.out.println("holaa");
+        
         System.out.println("Prueba para ver que todo funciona bien");
     }
 }
