b495fc668f18c5b27dc8f3683e4d3bb307e2133f
Windows10 <Windows10@DESKTOP-UBSSNMJ>
2021-05-27 22:33:54 -0500

    Lista la galeria de imagenes

---
+++ Galeria/src/galeria/Galeria.java
@@ -0,0 +1,15 @@
+
+package galeria;
+
+
+public class Galeria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Interfaz().setVisible(true);
+    }
+    
+}
